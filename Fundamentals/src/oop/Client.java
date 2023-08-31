286ab29b4b2e60857c0520e6b89e5ad96b76e0a7
CaioTakabatake <caio.soldera.taka@gmail.com>
2023-08-31 09:50:07 -0300

    sending oop class revision

--- Fundamentals/src/oop/Client.java
+++ Fundamentals/src/oop/Client.java
@@ -1,2 +1,53 @@
-package oop;public class Client {
+package oop;
+
+public class Client {
+    private String name;
+    private String email;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public Client(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double calculateRental(int numberOfDays) {
+        return numberOfDays * 3.99;
+    }
+
+    public double calculateRental(int numberOfDays, boolean isNewRelease) {
+        return numberOfDays * 6.99;
+    }
+
+    @Override
+    public String toString() {
+        if (this.email == null) {
+            return "Client{" +
+                    "name='" + name + '\'' +
+                    '}';
+        } else {
+            return "Client{" +
+                    "name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    '}';
+        }
+    }
 }
--- Fundamentals/src/oop/Rental.java
+++ Fundamentals/src/oop/Rental.java
@@ -1,4 +1,19 @@
 package oop;
 
+import java.io.PrintStream;
+
 public class Rental {
+    public static void main(String[] args) {
+        System.setOut(new PrintStream(System.out, true));
+
+        Client firstClient = new Client("Caio Takabatake");
+        System.out.println(firstClient);
+        double valueRentalSimple = firstClient.calculateRental(3);
+        System.out.println("The value for renting the movie is: " + valueRentalSimple);
+
+        Client secondClient = new Client("Caio Zein", "caiozein@gmail.com");
+        System.out.println(secondClient);
+        double valueRentalLRelease = secondClient.calculateRental(3, true);
+        System.out.println("The value for renting the movie is: " + valueRentalLRelease);
+    }
 }
